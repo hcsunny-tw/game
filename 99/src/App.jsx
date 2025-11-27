a41e138e1f06f5834f2dbc6eea6eb91c6840548d
hcsunny-tw <hc1998@go.edu.tw>
2025-11-27 20:29:00 +0800

    Create App.jsx

---
+++ 99/src/App.jsx
@@ -0,0 +1,11 @@
+import MathTerritoryGame from './components/MathTerritoryGame'; // 假設您將其放在 components 資料夾
+
+function App() {
+  return (
+    <div className="App">
+      <MathTerritoryGame />
+    </div>
+  );
+}
+
+export default App;
